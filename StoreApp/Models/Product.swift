e060ce18f67d1f2357adb823a30e068935e029d8
baris dilekci <bdilekci200@gmail.com>
2023-04-06 16:27:32 +0300

    Product model added.

---
+++ StoreApp/Models/Product.swift
@@ -0,0 +1,17 @@
+//
+//  Product.swift
+//  StoreApp
+//
+//  Created by Baris on 6.04.2023.
+//
+
+import Foundation
+
+struct Product : Codable {
+    let id: Int?
+    let title : String
+    let price : Double
+    let description: String
+    let images : [URL]?
+    let category : [Category]
+}
