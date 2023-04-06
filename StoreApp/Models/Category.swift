e11ff787c00b749216251c107728fd4c935e00ed
baris dilekci <bdilekci200@gmail.com>
2023-04-06 04:23:12 +0300

    The category model has been created.

---
+++ StoreApp/Models/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  StoreApp
+//
+//  Created by Baris on 6.04.2023.
+//
+
+import Foundation
+
+struct Category: Codable {
+    let id: Int
+    let name: String
+    let image : String
+}
